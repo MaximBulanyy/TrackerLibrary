5a43ec2112d3a5c6252aa0c36ed772014452788e
MoM <MoM@DESKTOP-O787KMO>
2024-04-06 19:58:49 +0500

    Добавьте файлы проекта.

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        public TeamModel TeamCompeting { get; set; }
+        public double Score { get; set; }
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
---
+++ TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupModel
+    {
+        public List<MatchupModel> Entries { get; set; } = new List<MatchupModel>();
+        public TeamModel Winner { get; set; }
+        public int MatchupRound { get; set; }
+    }
+}
